c438268aeca8d423554ec54375741e970ae2dfd7
Alex <akorolonek@yahoo.com>
2023-04-06 00:31:48 +0200

    Finished Program

--- programs/wsos-FacebookClone/src/lib.rs
+++ programs/wsos-FacebookClone/src/lib.rs
@@ -1,14 +1,12 @@
-
-
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Token};
 use std::mem::size_of;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 declare_id!("3gQfmeiQb9zfP4Utg22Vn2RJvzAwx264oaNrctRNd91W");
 
-const TEXT_LENGHT: usize = 1024;
-const USER_NAME_LENGHT: usize = 100;
-const USER_URL_LENGHT: usize = 255;
+const TEXT_LENGTH: usize = 1024;
+const USER_NAME_LENGTH: usize = 100;
+const USER_URL_LENGTH: usize = 255;
 
 #[program]
 pub mod programs {
@@ -31,72 +29,153 @@ pub mod programs {
         post.text = text;
         post.poster_name = poster_name;
         post.poster_url = poster_url;
-      //post.comment_count = 0;
+        post.comment_count = 0;
         post.index = state.post_count;
         post.post_time = ctx.accounts.clock.unix_timestamp;
 
         state.post_count += 1;
         Ok(())
+    }
+    pub fn create_comment(
+        ctx: Context<CreateComment>,
+        text: String,
+        commenter_name: String,
+        commenter_url: String,
+    ) -> ProgramResult {
 
+
+        let post = &mut ctx.accounts.post;
+        let comment = &mut ctx.accounts.comment;
+
+        comment.authority = ctx.accounts.authority.key();
+        comment.text = text;
+        comment.commenter_name = commenter_name;
+        comment.commenter_url = commenter_url;
+        comment.index = post.comment_count;
+        comment.post_time = ctx.accounts.clock.unix_timestamp;
+        post.comment_count += 1;
+
+        Ok(())
     }
 }
 
+    
+
+/// Contexts
+/// CreateState context
 #[derive(Accounts)]
 pub struct CreateState<'info> {
+    // Authenticating state account
     #[account(
-        init, 
+        init,
         seeds = [b"state".as_ref()],
         bump,
         payer = authority,
-        space = size_of::<StateAccount>()
+        space = size_of::<StateAccount>() + 8
     )]
     pub state: Account<'info, StateAccount>,
 
-    //Authority (Signer of the Transaction)
+    // Authority (this is signer who paid transaction fee)
     #[account(mut)]
     pub authority: Signer<'info>,
 
-    //System Program
+    /// System program
     pub system_program: UncheckedAccount<'info>,
-    
-    //Token Program
+
+    // Token program
     #[account(constraint = token_program.key == &token::ID)]
-    pub token_program: Program<'info, Token>
+    pub token_program: Program<'info, Token>,
 }
+
+/// CreatePost context
 #[derive(Accounts)]
 pub struct CreatePost<'info> {
-    #[account(mut, seeds = [b"state".as_ref()],bump)]
+    // Authenticate state account
+    #[account(mut, seeds = [b"state".as_ref()], bump)]
     pub state: Account<'info, StateAccount>,
 
-    //Authenticate Post Account
+    // Authenticate post account
     #[account(
         init,
-        seeds = [b"post".as_ref(),state.post_count.to_be_bytes().as_ref()],
+        // Post account use string "post" and index of post as seeds
+        seeds = [b"post".as_ref(), state.post_count.to_be_bytes().as_ref()],
         bump,
         payer = authority,
-        space = size_of::<PostAccount>() + USER_NAME_LENGHT + USER_URL_LENGHT + TEXT_LENGHT
+        space = size_of::<PostAccount>() + TEXT_LENGTH + USER_NAME_LENGTH + USER_URL_LENGTH
     )]
     pub post: Account<'info, PostAccount>,
+
+    // Authority (this is signer who paid transaction fee)
     #[account(mut)]
     pub authority: Signer<'info>,
+
+    /// System program
     pub system_program: UncheckedAccount<'info>,
+
+    // Token program
+    #[account(constraint = token_program.key == &token::ID)]
+    pub token_program: Program<'info, Token>,
+
+    // Clock to save time
+    pub clock: Sysvar<'info, Clock>,
+}
+
+/// CreateComment context
+#[derive(Accounts)]
+pub struct CreateComment<'info> {
+    // Authenticate post account
+    #[account(mut, seeds = [b"post".as_ref(), post.index.to_be_bytes().as_ref()], bump)]
+    pub post: Account<'info, PostAccount>,
+
+    // Authenticate comment account
+    #[account(
+        init,
+        // Post account use string "comment", index of post and index of comment per post as seeds
+        seeds = [b"comment".as_ref(), post.index.to_be_bytes().as_ref(), post.comment_count.to_be_bytes().as_ref()],
+        bump,
+        payer = authority,
+        space = size_of::<CommentAccount>() + TEXT_LENGTH + USER_NAME_LENGTH + USER_URL_LENGTH
+    )]
+    pub comment: Account<'info, CommentAccount>,
+
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    /// System program
+    pub system_program: UncheckedAccount<'info>,
+
     #[account(constraint = token_program.key == &token::ID)]
     pub token_program: Program<'info, Token>,
     pub clock: Sysvar<'info, Clock>,
 }
 
+// State Account Structure
 #[account]
 pub struct StateAccount {
     pub authority: Pubkey,
     pub post_count: u64,
 }
-//Post Account Struct
+
+// Post Account Structure
 #[account]
 pub struct PostAccount {
     pub authority: Pubkey,
     pub text: String,
     pub poster_name: String,
     pub poster_url: String,
+    pub comment_count: u64,
+    pub index: u64,
+    pub post_time: i64,
+}
+
+// Comment Account Structure
+#[account]
+pub struct CommentAccount {
+    pub authority: Pubkey,
+    pub text: String,
+    pub commenter_name: String,
+    pub commenter_url: String,
     pub index: u64,
     pub post_time: i64,
 }
